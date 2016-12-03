ca7cbfde9c78f71eef02c7436d200d8e217aba07
Emmanuel <brard.e@gmail.com>
2016-12-03 16:50:12 +0100

    Modifying documentation
    Modifying documentation to comply to Talend specification.

--- DateTransformations.java
+++ DateTransformations.java
@@ -8,16 +8,16 @@ import java.time.format.DateTimeFormatter ;
 public class DateTransformations {
 
     /**
-     * ChangeTimeZone: Date, Convert date string between time zones
+     * ChangeTimeZone: Date, Convert date string between UTC and a specific timezone
      * 
      * 
      * {talendTypes} Date
      * 
      * {Category} User Defined
      * 
-     * {param} string("Date") input: The string date need to be converted.
+     * {param} string("Date") input: The date as String that needs to be converted.
      * 
-     * {example} helloExemple("world") # hello world !.
+     * {example} ChangeTimeZone("2016-11-10 23:00", "yyyy-MM-dd HH:mm", "Europe/Zurich") # hello world !.
      */
 	   public static Date ChangeTimeZone(String DateAsString, String DateFormat, String TimeZoneOutput) {
 		   DateTimeFormatter formatter =
@@ -25,9 +25,6 @@ public class DateTransformations {
 		   
 		   ZonedDateTime VarDate = ZonedDateTime.parse(DateAsString, formatter);
 		            
-	       return Date.from(  ( VarDate.withZoneSameInstant( ZoneId.of(TimeZoneOutput)) ).toInstant() );  
-		   
-	   }
-   
-    
+	       return Date.from(  ( VarDate.withZoneSameInstant( ZoneId.of(TimeZoneOutput)) ).toInstant() );  	   
+	   }    
 }
